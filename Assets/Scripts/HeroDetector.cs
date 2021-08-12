1d235a4f4b22c000f95313534036860586004dc8
Steven Meiklejohn <stevenmeiklejohn700@gmail.com>
2021-08-12 11:49:30 +0100

    Initial Commit. Level sprites imported.

---
+++ Assets/Scripts/HeroDetector.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+[RequireComponent(typeof(Collider))]
+
+
+
+public class HeroDetector : MonoBehaviour{
+
+    public bool heroIsNearby;
+
+
+    public void OnTriggerEnter(Collider collider){
+        if (collider.tag == "Hero"){
+            heroIsNearby = true;
+        }
+    }
+
+
+    public void OnTriggerExit(Collider collider){
+        if (collider.tag == "Hero"){
+            heroIsNearby = false;
+        }
+    }
+}
