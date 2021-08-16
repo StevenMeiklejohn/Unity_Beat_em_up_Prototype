5a4eadb7aa909213b9a274bc9b0920fe54c746a1
Steven Meiklejohn <stevenmeiklejohn700@gmail.com>
2021-08-16 11:22:55 +0100

    Added AI scripts. Enemy moves towards player when -inSight-

---
+++ Assets/Scripts/Enemy/EnemySight.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySight : MonoBehaviour
+{
+  public bool playerInSight;
+  public GameObject player;
+  public GameObject target;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+      target = player;
+    }
+
+    void OnTriggerStay(Collider other){
+      if(other.gameObject == player){
+        playerInSight = true;
+      }
+
+    }
+
+    void OnTriggerExit(Collider other){
+      if(other.gameObject == player){
+        playerInSight = false;
+      }
+    }
+}
---
+++ Assets/Scripts/Enemy/EnemyWalk.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyWalk : MonoBehaviour
+{
+
+  NavMeshAgent navMeshAgent;
+  EnemySight enemySight;
+  public float enemyCurrentSpeed;
+  public float enemySpeed;
+
+
+    void Awake()
+    {
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        enemySight = GetComponent<EnemySight>();
+
+        navMeshAgent.speed = enemySpeed;
+    }
+
+
+    void Update()
+    {
+      if(enemySight.playerInSight == true){
+        navMeshAgent.SetDestination(enemySight.target.transform.position);
+        navMeshAgent.updateRotation = false;
+      }
+    }
+}
